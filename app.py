be7aa0fa95f57a3f42d9f6492031b84b027674e7
Pinja Vierimaa <pinja.vierimaa@helsinki.fi>
2020-11-07 23:18:04 +0200

    Sovelluksen tekeminen aloitettu.

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask import redirect, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from os import getenv
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
