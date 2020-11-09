548f81ba161fc079be8a66184a481df38e1300be
Pinja Vierimaa <pinja.vierimaa@helsinki.fi>
2020-11-09 15:38:17 +0200

    Kirjautumis- ja rekisteröitymistietojen validointi tehty.

--- app.py
+++ app.py
@@ -22,37 +22,42 @@ def basics():
 
 @app.route("/signin")
 def signinget():
-    return render_template("register.html")
+    return render_template("register.html", error="")
 
 @app.route("/register", methods=["POST"])
 def signinpost():
     username = request.form["username"]
-    password = request.form["password"]
-    hash_value = generate_password_hash(password)
-    sql = "INSERT INTO users (username, password) VALUES (:username, :password)"
-    db.session.execute(sql, {"username":username, "password":hash_value})
-    db.session.commit()
+    sql = "SELECT password FROM users WHERE username=:username"
+    result = db.session.execute(sql, {"username":username})
+    user = result.fetchone()    
+    if user == None:
+        password = request.form["password"]
+        hash_value = generate_password_hash(password)
+        sql = "INSERT INTO users (username, password) VALUES (:username, :password)"
+        db.session.execute(sql, {"username":username, "password":hash_value})
+        db.session.commit()
+    else:
+        return render_template("register.html", error="Käyttäjänimi on jo varattu.")
     return redirect("/")
 
 @app.route("/login")
 def loginget():
-    return render_template("login.html")
+    return render_template("login.html", usernameError="", passwordError="")
 
 @app.route("/login", methods=["POST"])
 def loginpost():
     username = request.form["username"]
     password = request.form["password"]
-    #sql = "SELECT password FROM users WHERE username=:username"
-    #result = db.session.execute(sql, {"username":username})
-    #user = result.fetchone()    
-    #if user == None:
-        ## TODO: invalid username
-    #else:
-        #hash_value = user[0]
-        #if check_password_hash(hash_value,password):
-            ## Kaikki OK
-        #else:
-            ## TODO: invalid password
+    sql = "SELECT password FROM users WHERE username=:username"
+    result = db.session.execute(sql, {"username":username})
+    user = result.fetchone()    
+    if user == None:
+        # TODO: invalid username
+        return render_template("login.html", usernameError="Käyttäjänimi on väärin.")
+    else:
+        hash_value = user[0]
+        if not check_password_hash(hash_value,password):
+            return render_template("login.html", passwordError="Salasana on väärin.")
     session["username"] = username
     return redirect("/")
 
